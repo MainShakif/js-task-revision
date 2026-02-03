dc7d7fde9e02b48c28052d08f10d2cfa7dbbc341
MainShakif <shakifahmed27@gmail.com>
2026-02-03 09:51:38 +0600

    first commit

---
+++ intro-to-js/fruitsPrice.js
@@ -0,0 +1,15 @@
+/*
+
+?Task-1
+You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return. 
+ */
+
+// TODO Solution
+let myMoney = 1000;
+let apple = 350;
+let orange = 250;
+let fruitsTotalPrice = apple + orange;
+console.log("Total Price is:", fruitsTotalPrice);
+
+let theShopkeeperWillReturn = myMoney - fruitsTotalPrice;
+console.log("The shopkeeper will return:", theShopkeeperWillReturn);
---
+++ intro-to-js/showRemainder.js
@@ -0,0 +1,17 @@
+/*
+?Task-3
+You task is to divide the given number by 5 and show the remainder as the output.
+
+Input:
+The first line of the input contains the number.
+
+Output:
+Print the remainder.
+*/
+
+//TODO Solution:
+
+let numberIs = 119;
+let givenNumberIs = 2;
+let remainderIs = numberIs % givenNumberIs;
+console.log("The remainder is:", remainderIs);
