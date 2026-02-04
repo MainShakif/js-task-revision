823f79f38a90b45e8c9c3988a45327e43fdea932
MainShakif <shakifahmed27@gmail.com>
2026-02-04 21:44:48 +0600

    learn array methods.

---
+++ module-14.5-Array-1/is-array.js
@@ -0,0 +1,5 @@
+let fruits = ["mango", "dango", "jango", "hengo", "lango"];
+let num = [1, 3, 54, 67, 91, 96];
+let a = true;
+
+console.log(Array.isArray(a));
--- module-14.5-Array-1/length.js
+++ module-14.5-Array-1/length.js
@@ -1,5 +1,6 @@
 const numbers = [10, 20, 30, 40, 50, 10, 20, 30, 40, 50, 10, 20, 30, 40, 50];
 console.log(numbers);
+console.log(numbers[-1]);
 
 /*---------------------------------------------
                 check length:
